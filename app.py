21ef50fd10fe0c0f9466464d6ad28519b2163dd3
etomiello <eftomiello@gmail.com>
2023-05-11 18:05:18 -0400

    added temp pfp

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__, template_folder='templates')
+app.secret_key = '354545452'
 
 #users = {}
 client = MongoClient('mongodb://mongo:27017')
@@ -37,12 +38,31 @@ def login():
         password = request.form.get('password')
 
         user = users.find_one({'username': username})
+        
         if user and check_password_hash(user['password'], password):
+            session['username'] = username  # Store the username in the session
             return redirect(url_for('feed'))
         else:
             return "Invalid username/password. Please try again."
 
     return render_template('login.html')
+
+@app.route('/account')
+def account():
+    # Get the username of the logged-in user from the session or wherever it is stored
+    username = session.get('username')
+    
+    # Fetch the user's account information from the database
+    user = users.find_one({'username': username})
+    
+    if user:
+        # Get the current auctions and total auctions won for the user (example data)
+        current_auctions = ['Auction 1', 'Auction 2', 'Auction 3']
+        total_auctions_won = 5
+    
+        return render_template('account.html', username=user['username'], current_auctions=current_auctions, total_auctions_won=total_auctions_won)
+    else:
+        return "User not found."  # Or redirect to an error page
 
 
 @app.route('/feed')
