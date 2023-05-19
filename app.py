d185368b93cc6e1a4164a18564b424ee90a25159
yatincs17 <yatinvij02@gmail.com>
2023-05-19 17:56:10 -0400

    ssh

--- app.py
+++ app.py
@@ -85,20 +85,17 @@ def echo(sock):
 
 @app.route('/account')
 def account():
-    # Get the username of the logged-in user from the session or wherever it is stored
     username = session.get('username')
     
-    # Fetch the user's account information from the database
     user = users.find_one({'username': username})
     
     if user:
-        # Get the current auctions and total auctions won for the user (example data)
         current_auctions = ['Auction 1', 'Auction 2', 'Auction 3']
         total_auctions_won = 5
     
         return render_template('account.html', username=user['username'], current_auctions=current_auctions, total_auctions_won=total_auctions_won)
     else:
-        return "User not found."  # Or redirect to an error page
+        return "User not found." 
 
 
 @app.route('/feed')
